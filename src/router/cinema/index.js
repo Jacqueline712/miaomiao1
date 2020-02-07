e843365e9f000e48e7cedb1628f03e305f1b95d8
unknown <you@example.com>
2020-02-07 17:04:42 +0800

    hello

---
+++ src/router/cinema/index.js
@@ -0,0 +1,6 @@
+export default{
+	path:'/cinema',
+	// 采用按需载入,箭头函数
+	// 在vue中@表示src目录
+	component:()=>import('@/views/Cinema')
+}
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		hello Movie
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
